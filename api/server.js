2076af19f38b7a33f070219de0183a703f220d5c
Talent Antonio <antonio.talenty@gmail.com>
2019-04-09 10:45:35 +0100

    to the database

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const session = require('express-session')
+const knexSessionStore = require('connect-session-knex')(session)
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
@@ -17,6 +18,14 @@ const sessionConfig = {
  httpOnly: true, 
  resave: false,
  saveUninitialized: false,
+ // storing in the data base
+ store: new knexSessionStore({
+   knex:require('../database/dbConfig'),
+   tablename: 'sessions',
+   sidfieldname: 'sid',
+   createtable: true,
+   clearInterval: 1000 * 60 * 60
+ })
 }
 
 server.use(session(sessionConfig));
